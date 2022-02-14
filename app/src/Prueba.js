0e8beb583021a16e6e94f5c27f891766bd4c9b0c
ClaudioGuevara <claudio.guevara.dev@gmail.com>
2022-02-13 23:13:41 -0300

    mezclamos el dashboard con la libreria blasterjs

--- app/src/Prueba.js
+++ app/src/Prueba.js
@@ -1,6 +1,82 @@
-import { useEffect } from 'react'
-
 import blasterjs from 'biojs-vis-blasterjs'
+
+import { useState, useEffect } from "react";
+
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Collapse from "@mui/material/Collapse";
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+import AppsIcon from "@mui/icons-material/Apps";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import {
+  styled,
+  ThemeProvider,
+  createTheme,
+  useTheme,
+} from "@mui/material/styles";
+
+const mdTheme = createTheme();
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
 
 let alignments = `
 BLASTP 2.8.1+
@@ -313,6 +389,10 @@ Window for multiple hits: 40
 `
 
 function Prueba() {
+  const theme = useTheme();
+
+  const [open, setOpen] = useState(false);
+  const [expandItemApps, setExpandItemApps] = useState(false);
 
   useEffect(() => {
     new blasterjs({
@@ -323,13 +403,85 @@ function Prueba() {
    });
   }, [])
 
-  return (
-    <div>
-      <div id="blast-multiple-alignments"></div>
-      <div id="blast-alignments-table"></div>
-      <div id="blast-single-alignment"></div>
-    </div>
-  );
+    return (
+      <ThemeProvider theme={mdTheme}>
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <AppBar position="fixed" open={open}>
+            <Toolbar
+              sx={{
+                pr: "24px",
+              }}
+            >
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={() => setOpen(true)}
+                sx={{
+                  mr: 2,
+                  ...(open && { display: "none" }),
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap component="div">
+                Dashboard
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            sx={{
+              width: drawerWidth,
+              flexShrink: 0,
+              "& .MuiDrawer-paper": {
+                width: drawerWidth,
+                boxSizing: "border-box",
+              },
+            }}
+            variant="persistent"
+            anchor="left"
+            open={open}
+          >
+            <DrawerHeader>
+              <IconButton onClick={() => setOpen(false)}>
+                {theme.direction === "ltr" ? (
+                  <ChevronLeftIcon />
+                ) : (
+                  <ChevronRightIcon />
+                )}
+              </IconButton>
+            </DrawerHeader>
+            <Divider />
+            <List>
+              <ListItemButton onClick={() => setExpandItemApps(!expandItemApps)}>
+                <ListItemIcon>
+                  <AppsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Tools" />
+                {expandItemApps ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={expandItemApps} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <FormatAlignCenterIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Alignments" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+            </List>
+          </Drawer>
+          <Main open={open}>
+            <DrawerHeader />
+            <div id="blast-multiple-alignments"></div>
+          <div id="blast-alignments-table"></div>
+          <div id="blast-single-alignment"></div>
+          </Main>
+        </Box>
+      </ThemeProvider>
+    );
 }
 
 export default Prueba;
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -154,21 +154,6 @@ const Dashboard = () => {
         </Drawer>
         <Main open={open}>
           <DrawerHeader />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
         </Main>
       </Box>
     </ThemeProvider>
