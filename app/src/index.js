4f0101cc8643d3ca67a71ba3859a5b5a461c4bd8
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-02-20 16:53:42 -0300

    listo para la reunión del 20 de febrero

--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// import 'bootswatch/dist/zephyr/bootstrap.min.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "proseqviewer/dist/assets/proseqviewer.css";
 
