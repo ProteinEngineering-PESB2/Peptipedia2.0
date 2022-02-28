34c59465bf5d67762c695505adfd355257547cd2
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-02-28 16:10:05 -0300

    sección del msa lista. Separamos la sección del alignment en dos

---
+++ app/src/components/Dashboard/Alignment/MSA/MSAForm.js
@@ -0,0 +1,28 @@
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+
+import Form from "./Form";
+
+const MSAForm = ({ setData, setError, setSeverity }) => {
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={8} lg={6} sx={{ margin: "start" }}>
+        <Paper
+          sx={{
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Form
+            setData={setData}
+            setError={setError}
+            setSeverity={setSeverity}
+          />
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default MSAForm;
--- app/src/components/Dashboard/Alignment/MSA/index.js
+++ app/src/components/Dashboard/Alignment/MSA/index.js
@@ -1,7 +1,36 @@
-const MSA = () => {
-    return (
-        <div>MSA</div>
-    )
-}
+import { useState } from "react";
 
-export default MSA
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+import MSAForm from "./MSAForm";
+import MSAContent from "./MSAContent";
+
+import Snackbar from "../../Snackbar";
+
+const MSA = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  const [severity, setSeverity] = useState("");
+
+  return (
+    <>
+      <Grid container spacing={5}>
+        {error.length > 0 && <Snackbar severity={severity} message={error} />}
+        <Grid item lg={12} xs={12}>
+          <Typography variant="h4">MSA Alignment</Typography>
+        </Grid>
+        <Grid item lg={12} xs={12}>
+          <MSAForm
+            setData={setData}
+            setError={setError}
+            setSeverity={setSeverity}
+          />
+        </Grid>
+        {data.length > 0 && <MSAContent data={data}/>}
+      </Grid>
+    </>
+  );
+};
+
+export default MSA;
