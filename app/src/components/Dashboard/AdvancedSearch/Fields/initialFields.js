96b3708deafb2d94c575378d4e345b699de6bce5
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-03-06 00:11:41 -0300

    avanzamos el advanced search

---
+++ app/src/components/Dashboard/AdvancedSearch/Fields/initialFields.js
@@ -0,0 +1,12 @@
+export const initialFields = [
+    { label: "Length" },
+    { label: "Molecular Weight" },
+    { label: "Isoelectric Point" },
+    { label: "Charge" },
+    { label: "Charge Density" },
+    { label: "Activity" },
+    { label: "Database" },
+    { label: "Gene Ontology" },
+    { label: "Pfam" },
+    { label: "Taxonomy" },
+]
