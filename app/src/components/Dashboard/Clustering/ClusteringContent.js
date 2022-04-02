7337f8ae57cb9d285b05c509b5ea1247cfa65f2c
ClaudioGuevaraDev <claudioguevaradev@gmail.com>
2022-04-01 21:14:28 -0300

    Agregamos pie chart al clustering

--- app/src/components/Dashboard/Clustering/ClusteringContent.js
+++ app/src/components/Dashboard/Clustering/ClusteringContent.js
@@ -1,3 +1,5 @@
+import Plot from "react-plotly.js";
+
 import { useEffect, useState } from "react";
 
 import Grid from "@mui/material/Grid";
@@ -10,6 +12,8 @@ const columns = ["Sequence", "Label"];
 
 const ClusteringContent = ({ res }) => {
   const [data, setData] = useState([]);
+  const [values, setValues] = useState([]);
+  const [labels, setLabels] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -22,6 +26,16 @@ const ClusteringContent = ({ res }) => {
     });
     setData(dataTable);
 
+    const valuesChart = [];
+    const labelsChart = [];
+
+    res.resume.forEach((r) => {
+      valuesChart.push(r.value);
+      labelsChart.push(`Cluster ${r.category}`);
+    });
+    setValues(valuesChart);
+    setLabels(labelsChart);
+
     setLoading(false);
   }, [res]);
 
@@ -30,49 +44,82 @@ const ClusteringContent = ({ res }) => {
       {loading ? (
         <div></div>
       ) : (
-        <Grid container spacing={3}>
-          <Grid item lg={12} md={12} xs={12}>
-            <Paper
-              sx={{
-                p: 2,
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <DataTable
-                title="Clustering Table"
-                data={data}
-                columns={columns}
-              />
-            </Paper>
-          </Grid>
-          <Grid item lg={12} md={12} xs={12}>
-            <Typography variant="h6">Clustering Performance</Typography>
-          </Grid>
-          <Grid item lg={6} md={8} xs={12}>
-            <div className="table-responsive">
-              <table
-                className="table table-light table-hover text-center"
-                style={{ width: "100%" }}
+        <>
+          <Grid container spacing={3}>
+            <Grid item lg={12} md={12} xs={12}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                }}
               >
-                <thead>
-                  <tr>
-                    <th>Calinski</th>
-                    <th>Dalvies</th>
-                    <th>Siluetas</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="table-active">
-                    <td>{res.performance.calinski}</td>
-                    <td>{res.performance.dalvies}</td>
-                    <td>{res.performance.siluetas}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+                <DataTable
+                  title="Clustering Table"
+                  data={data}
+                  columns={columns}
+                />
+              </Paper>
+            </Grid>
+            <Grid item lg={12} md={12} xs={12}>
+              <Typography variant="h6">Clustering Performance</Typography>
+            </Grid>
+            <Grid item lg={6} md={8} xs={12}>
+              <div className="table-responsive">
+                <table
+                  className="table table-light table-hover text-center"
+                  style={{ width: "100%" }}
+                >
+                  <thead>
+                    <tr>
+                      <th>Calinski</th>
+                      <th>Dalvies</th>
+                      <th>Siluetas</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="table-active">
+                      <td>{res.performance.calinski}</td>
+                      <td>{res.performance.dalvies}</td>
+                      <td>{res.performance.siluetas}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
+          <Grid container spacing={3} sx={{ marginTop: 2 }}>
+            <Grid item lg={6} md={8} xs={12}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Plot
+                  data={[
+                    {
+                      values: values,
+                      labels: labels,
+                      type: "pie",
+                      textinfo: "label+percent",
+                      textposition: "outside",
+                      automargin: true,
+                    },
+                  ]}
+                  layout={{
+                    autosize: true,
+                    height: 400,
+                    margin: {"t": 0, "b": 0, "l": 0, "r": 0},
+                  }}
+                  useResizeHandler
+                  className="w-full"
+                />
+              </Paper>
+            </Grid>
+          </Grid>
+        </>
       )}
     </>
   );
