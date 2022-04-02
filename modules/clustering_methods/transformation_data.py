a43c2d811cb387f7b4c739566a9d47f0e34c49ea
dMedinaO <41024522+dMedinaO@users.noreply.github.com>
2022-04-01 16:00:23 -0800

    Add transform instance

---
+++ modules/clustering_methods/transformation_data.py
@@ -0,0 +1,15 @@
+from sklearn.decomposition import PCA
+from sklearn.decomposition import KernelPCA
+
+class transformer(object):
+    
+    def apply_pca_data(self, dataset):
+
+        #instance of PCA
+        pca_transformer = PCA()
+        return pca_transformer.fit_transform(dataset)
+
+    def apply_kernel_pca(self, dataset, kernel):
+
+        transformer_instance = KernelPCA(kernel=kernel)
+        return transformer_instance.fit_transform(dataset)
