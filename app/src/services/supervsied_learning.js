f63a59093d5b6255b268f5329bbb89f4ff1b43f2
GabrielCabas <gabrielcabas123>
2022-04-22 17:19:27 -0400

    agregué un slash para arreglar el problem

--- app/src/services/supervsied_learning.js
+++ app/src/services/supervsied_learning.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const supervisedLearning = async (post) => {
-  const res = await axios.post("/api/supervised_learning", post);
-
+  const res = await axios.post("/api/supervised_learning/", post);
   return res.data;
 };
