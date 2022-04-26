807764612b55d0223b5c9c2ad03ca5d2b5955d50
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-04-26 11:11:26 -0400

    menu qlo

--- app/src/pages/Dashboard/index.js
+++ app/src/pages/Dashboard/index.js
@@ -7,7 +7,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
 
 // Components
 import ListItems from "../../components/Dashboard/ListItems";
@@ -20,41 +20,67 @@ const Dashboard = () => {
   const [section, setSection] = useState("home");
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: drawerWidth,
-            boxSizing: "border-box",
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <ListItems setSection={setSection} section={section} />
+    <>
+      <Container maxWidth="xl">
+        <IconButton
+          aria-label="open drawer"
+          edge="start"
+          onClick={() => setOpen(!open)}
+          size="large"
+          sx={{
+            mr: 2,
+            color: "#000",
+          }}
+        >
+          <FormatAlignJustifyIcon />
+        </IconButton>
+        <RenderSection section={section} />
+      </Container>
+      <Drawer open={open} onClose={() => setOpen(false)}>
+        <Box
+          role="presentation"
+          onClick={() => setOpen(false)}
+          onKeyDown={() => setOpen(false)}
+        >
+          <ListItems setSection={setSection} section={section} />
+        </Box>
       </Drawer>
-      <Main open={open}>
-        <Container maxWidth="false">
-          <IconButton
-            aria-label="open drawer"
-            edge="start"
-            onClick={() => setOpen(!open)}
-            size="large"
-            sx={{
-              mr: 2,
-              color: "#000",
-            }}
-          >
-            <FormatAlignJustifyIcon />
-          </IconButton>
-          <RenderSection section={section} />
-        </Container>
-      </Main>
-    </Box>
+    </>
+    // <Box sx={{ display: "flex" }}>
+    //   <CssBaseline />
+    //   <Drawer
+    //     sx={{
+    //       width: drawerWidth,
+    //       flexShrink: 0,
+    //       "& .MuiDrawer-paper": {
+    //         width: drawerWidth,
+    //         boxSizing: "border-box",
+    //       },
+    //     }}
+    //     variant="persistent"
+    //     anchor="left"
+    //     open={open}
+    //   >
+    //     <ListItems setSection={setSection} section={section} />
+    //   </Drawer>
+    //   <Main open={open}>
+    //     <Container maxWidth="false">
+    //       <IconButton
+    //         aria-label="open drawer"
+    //         edge="start"
+    //         onClick={() => setOpen(!open)}
+    //         size="large"
+    //         sx={{
+    //           mr: 2,
+    //           color: "#000",
+    //         }}
+    //       >
+    //         <FormatAlignJustifyIcon />
+    //       </IconButton>
+    //       <RenderSection section={section} />
+    //     </Container>
+    //   </Main>
+    // </Box>
   );
 };
 
