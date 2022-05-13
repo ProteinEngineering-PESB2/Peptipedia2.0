e9fd62243040d27c2a3b925eb46a6d38dff1002e
GabrielCabas <gabrielcabas123>
2022-05-13 01:09:40 -0400

    fasta convertor corregido

--- backend/src/modules/fasta_convertor.py
+++ backend/src/modules/fasta_convertor.py
@@ -4,15 +4,19 @@ class fasta_convertor:
         self.text = text
         self.limit = limit
         self.fasta_path = static + "/" + str(round(random()*10**20)) + ".fasta"
+        self.alphabet = ["A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"]
     
     def convert(self):
         self.fasta_text = ""
         splitted = [a.strip() for a in self.text.split("\n") if a.strip() != ""]
         for i, row in enumerate(splitted):
-            res = ">sequence " + str(i) + "\n" + "\n".join(
-                [row[y - self.limit:y] for y in range(self.limit, len(row) + self.limit, self.limit)]
-                ) + "\n"
-            self.fasta_text += res
+            row = "".join([a for a in row if a in self.alphabet or a.upper() in self.alphabet])
+            row = row.upper()
+            if len(row) > 0:
+                res = ">sequence " + str(i) + "\n" + "\n".join(
+                    [row[y - self.limit:y] for y in range(self.limit, len(row) + self.limit, self.limit)]
+                    ) + "\n"
+                self.fasta_text += res
         return self.fasta_text
     
     def save_file(self):
