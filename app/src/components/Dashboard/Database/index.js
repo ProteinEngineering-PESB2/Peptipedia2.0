76291dcade9cefb8958a2f08318086e80acb765c
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-05-14 13:36:18 -0400

    estadística de la base de datos lista

--- app/src/components/Dashboard/Database/index.js
+++ app/src/components/Dashboard/Database/index.js
@@ -1,48 +1,21 @@
-import { Grid, Typography, Button } from "@mui/material";
+import { useCallback, useEffect } from "react";
+import { Grid, Typography, Button, Link } from "@mui/material";
 import axios from "axios";
 import { useStateIfMounted } from "use-state-if-mounted";
-import Tree from "react-d3-tree";
+import DataTable from "../DataTable";
 
 import LoadingButton from "@mui/lab/LoadingButton";
 import SnackbarComponent from "../Snackbar";
-
-const orgChart = {
-  name: "CEO",
-  children: [
-    {
-      name: "Manager",
-      attributes: {
-        department: "Production",
-      },
-      children: [
-        {
-          name: "Foreman",
-          attributes: {
-            department: "Fabrication",
-          },
-          children: [
-            {
-              name: "Worker",
-            },
-          ],
-        },
-        {
-          name: "Foreman",
-          attributes: {
-            department: "Assembly",
-          },
-          children: [
-            {
-              name: "Worker",
-            },
-          ],
-        },
-      ],
-    },
-  ],
-};
+import CircularLoading from "../CircularLoading";
 
 const Database = () => {
+  const [loading, setLoading] = useStateIfMounted(true);
+  const [columnsDBStatistics, setColumnsDBStatistics] = useStateIfMounted([]);
+  const [dataDBStatistics, setDataDBStatistics] = useStateIfMounted([]);
+  const [open, setOpen] = useStateIfMounted(false);
+  const [message, setMessage] = useStateIfMounted("");
+  const [severity, setSeverity] = useStateIfMounted("");
+
   const [loadingZIP, setLoadingZIP] = useStateIfMounted(false);
   const [loadingSQL, setLoadingSQL] = useStateIfMounted(false);
   const [loadingFasta, setLoadingFasta] = useStateIfMounted(false);
@@ -59,6 +32,38 @@ const Database = () => {
   const [snackbarFastaMessage, setSnackbarFastaMessage] = useStateIfMounted("");
   const [snackbarFastaSeverity, setSnackbarFastaSeverity] =
     useStateIfMounted("");
+
+  const getDBStatistics = async () => {
+    try {
+      const res = await axios.get("/api/get_db_statistics/");
+
+      const new_data = [];
+      for (let i = 0; i < res.data.data.length; i++) {
+        if (res.data.data[i].length === 3) {
+          const parcial_data = [
+            res.data.data[i][0],
+            res.data.data[i][1],
+            <Link href={res.data.data[i][2]} target="_blank">
+              {res.data.data[i][2]}
+            </Link>,
+          ];
+          new_data.push(parcial_data);
+        }
+      }
+
+      setColumnsDBStatistics(res.data.columns);
+      setDataDBStatistics(new_data);
+    } catch (error) {
+      setSeverity("error");
+      setMessage("Service no available");
+      setOpen(true);
+    }
+  };
+
+  useEffect(() => {
+    getDBStatistics();
+    setLoading(false);
+  }, []);
 
   const downloadZIP = async () => {
     try {
@@ -152,110 +157,134 @@ const Database = () => {
 
   return (
     <>
-      <Grid container spacing={2}>
-        {snackbarZIPMessage.length > 0 && (
-          <SnackbarComponent
-            open={openSnackbarZIP}
-            setOpen={setOpenSnackbarZIP}
-            message={snackbarZIPMessage}
-            severity={snackbarZIPSeverity}
-          />
-        )}
-        {snackbarSQLMessage.length > 0 && (
-          <SnackbarComponent
-            open={openSnackbarSQL}
-            setOpen={setOpenSnackbarSQL}
-            message={snackbarSQLMessage}
-            severity={snackbarSQLSeverity}
-            time={20000}
-          />
-        )}
-        {snackbarFastaMessage.length > 0 && (
-          <SnackbarComponent
-            open={openSnackbarFasta}
-            setOpen={setOpenSnackbarFasta}
-            message={snackbarFastaMessage}
-            severity={snackbarFastaSeverity}
-          />
-        )}
-        <Grid item lg={12} md={12} xs={12}>
-          <Typography variant="h4" style={{ fontWeight: "bold" }}>
-            Peptipedia Database
-          </Typography>
-        </Grid>
-        <Grid item lg={12} md={12} xs={12}>
-          <Tree data={orgChart}/>
-        </Grid>
-        <Grid item lg={2.6} md={3.5} sm={4} xs={12}>
-          {loadingZIP ? (
-            <LoadingButton
-              loading
-              variant="contained"
-              sx={{ width: "100%" }}
-              size="medium"
-            >
-              Loading{" "}
-            </LoadingButton>
-          ) : (
-            <Button
-              variant="contained"
-              size="large"
-              sx={{
-                backgroundColor: "#2962ff",
-                width: "100%",
-                ":hover": { backgroundColor: "#2962ff" },
-              }}
-              onClick={downloadZIP}
-            >
-              Download as CSV
-            </Button>
-          )}
-        </Grid>
-        <Grid item lg={2.6} md={3.5} sm={4} xs={12}>
-          {loadingSQL ? (
-            <LoadingButton
-              loading
-              variant="contained"
-              sx={{ width: "100%" }}
-              size="medium"
-            >
-              Loading{" "}
-            </LoadingButton>
-          ) : (
-            <Button
-              variant="contained"
-              size="large"
-              color="warning"
-              sx={{ width: "100%" }}
-              onClick={downloadSQL}
-            >
-              Download as SQL
-            </Button>
-          )}
-        </Grid>
-        <Grid item lg={2.6} md={3.5} sm={4} xs={12}>
-          {loadingFasta ? (
-            <LoadingButton
-              loading
-              variant="contained"
-              sx={{ width: "100%" }}
-              size="medium"
-            >
-              Loading{" "}
-            </LoadingButton>
-          ) : (
-            <Button
-              variant="contained"
-              size="large"
-              color="secondary"
-              sx={{ width: "100%" }}
-              onClick={downloadFasta}
-            >
-              Download as Fasta
-            </Button>
-          )}
-        </Grid>
-      </Grid>
+      {loading ? (
+        <CircularLoading />
+      ) : (
+        <>
+          <Grid container spacing={2}>
+            {message.length > 0 && (
+              <SnackbarComponent
+                open={open}
+                setOpen={setOpen}
+                message={message}
+                severity={severity}
+              />
+            )}
+            {snackbarZIPMessage.length > 0 && (
+              <SnackbarComponent
+                open={openSnackbarZIP}
+                setOpen={setOpenSnackbarZIP}
+                message={snackbarZIPMessage}
+                severity={snackbarZIPSeverity}
+              />
+            )}
+            {snackbarSQLMessage.length > 0 && (
+              <SnackbarComponent
+                open={openSnackbarSQL}
+                setOpen={setOpenSnackbarSQL}
+                message={snackbarSQLMessage}
+                severity={snackbarSQLSeverity}
+                time={20000}
+              />
+            )}
+            {snackbarFastaMessage.length > 0 && (
+              <SnackbarComponent
+                open={openSnackbarFasta}
+                setOpen={setOpenSnackbarFasta}
+                message={snackbarFastaMessage}
+                severity={snackbarFastaSeverity}
+              />
+            )}
+            <Grid item lg={12} md={12} xs={12}>
+              <Typography variant="h4" style={{ fontWeight: "bold" }}>
+                Peptipedia Database
+              </Typography>
+            </Grid>
+            <Grid item lg={12} md={12} xs={12}>
+              <Typography variant="h6">Download Database</Typography>
+            </Grid>
+            <Grid item lg={2.6} md={3.5} sm={4} xs={12}>
+              {loadingZIP ? (
+                <LoadingButton
+                  loading
+                  variant="contained"
+                  sx={{ width: "100%" }}
+                  size="medium"
+                >
+                  Loading{" "}
+                </LoadingButton>
+              ) : (
+                <Button
+                  variant="contained"
+                  size="large"
+                  sx={{
+                    backgroundColor: "#2962ff",
+                    width: "100%",
+                    ":hover": { backgroundColor: "#2962ff" },
+                  }}
+                  onClick={downloadZIP}
+                >
+                  Download as CSV
+                </Button>
+              )}
+            </Grid>
+            <Grid item lg={2.6} md={3.5} sm={4} xs={12}>
+              {loadingSQL ? (
+                <LoadingButton
+                  loading
+                  variant="contained"
+                  sx={{ width: "100%" }}
+                  size="medium"
+                >
+                  Loading{" "}
+                </LoadingButton>
+              ) : (
+                <Button
+                  variant="contained"
+                  size="large"
+                  color="warning"
+                  sx={{ width: "100%" }}
+                  onClick={downloadSQL}
+                >
+                  Download as SQL
+                </Button>
+              )}
+            </Grid>
+            <Grid item lg={2.6} md={3.5} sm={4} xs={12}>
+              {loadingFasta ? (
+                <LoadingButton
+                  loading
+                  variant="contained"
+                  sx={{ width: "100%" }}
+                  size="medium"
+                >
+                  Loading{" "}
+                </LoadingButton>
+              ) : (
+                <Button
+                  variant="contained"
+                  size="large"
+                  color="secondary"
+                  sx={{ width: "100%" }}
+                  onClick={downloadFasta}
+                >
+                  Download as Fasta
+                </Button>
+              )}
+            </Grid>
+            <Grid item lg={12} md={12} xs={12}>
+              <Typography variant="h6">Database Statistics</Typography>
+            </Grid>
+            <Grid item lg={12} md={12} xs={12}>
+              <DataTable
+                title="Database Statistics"
+                columns={columnsDBStatistics}
+                data={dataDBStatistics}
+              />
+            </Grid>
+          </Grid>
+        </>
+      )}
     </>
   );
 };
