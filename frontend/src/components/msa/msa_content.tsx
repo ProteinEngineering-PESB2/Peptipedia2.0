4cc79d3f2fd9ef9c6ea64c8c9acb78d856761126
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-06-12 15:39:31 -0400

    pasamos el proseqviewer a un custom hook

--- frontend/src/components/msa/msa_content.tsx
+++ frontend/src/components/msa/msa_content.tsx
@@ -1,28 +1,29 @@
 import { Box, Paper } from "@mui/material";
 import { useEffect } from "react";
+import { useProSeqViewer } from "../../hooks/useProSeqViewer";
 import { IAlign } from "../../utils/interfaces";
-import { ProSeqViewer } from "proseqviewer/dist";
 
 interface Props {
   result: IAlign[];
 }
 
-const options = {
-  sequenceColor: "clustal",
-  wrapLine: false,
-  chunkSize: 20,
-  viewerWidth: "100%"
-};
-
 export default function MSAContent({ result }: Props) {
-  useEffect(() => {
-    const psv = new ProSeqViewer("psv");
-    psv.draw({ sequences: result, options });
-  }, []);
+  useProSeqViewer({ sequences: result });
 
   return (
     <>
-      <Box marginTop={3} sx={{ maxWidth: { xs: "20.8rem", sm: "100%", md: "100%", lg: "100%", xl: "100%" } }}>
+      <Box
+        marginTop={3}
+        sx={{
+          maxWidth: {
+            xs: "20rem",
+            sm: "100%",
+            md: "100%",
+            lg: "100%",
+            xl: "100%",
+          },
+        }}
+      >
         <Paper
           sx={{
             p: 2,
---
+++ frontend/src/hooks/useProSeqViewer.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { IAlign } from "../utils/interfaces";
+import { ProSeqViewer } from "proseqviewer/dist";
+
+interface Props {
+  sequences: IAlign[];
+}
+
+const options = {
+  sequenceColor: "clustal",
+  wrapLine: false,
+  chunkSize: 20,
+  viewerWidth: "100%",
+};
+
+export const useProSeqViewer = ({ sequences }: Props) => {
+  useEffect(() => {
+    const psv = new ProSeqViewer("psv");
+    psv.draw({ sequences, options });
+  }, []);
+
+  return {};
+};
