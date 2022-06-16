200559160943ac73e3b42b2a11a91d298f109c65
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-06-15 22:33:24 -0400

    formulario de la frequency evaluation listo

---
+++ frontend/src/components/frequency/frequency_form.tsx
@@ -0,0 +1,52 @@
+import { FormEvent, useState } from "react";
+import toast from "react-hot-toast";
+import { parserFormDataWithoutOptions } from "../../helpers/parserFormData";
+import { requestPost } from "../../services/api";
+import { InitialValuePostData } from "../../utils/initial_values";
+import { PostData } from "../../utils/interfaces";
+import ButtonRun from "../form/button_run";
+import FormContainer from "../form/form_container";
+import InputFileFasta from "../form/input_file_fasta";
+import InputFileType from "../form/input_file_type";
+import TextFieldFasta from "../form/text_field_fasta";
+
+export default function FrequencyForm() {
+  const [data, setData] = useState<PostData>(InitialValuePostData);
+  const [openBackdrop, setOpenBackdrop] = useState<boolean>(false);
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+    setOpenBackdrop(true);
+
+    const postData = parserFormDataWithoutOptions(data);
+
+    try {
+      const { data } = await requestPost({
+        url: "/api/frequency",
+        postData,
+      });
+
+      if (data.status === "error") {
+        toast.error(data.description);
+      } else {
+        console.log(data);
+      }
+    } catch (error) {
+      toast.error("Server error");
+      setOpenBackdrop(false);
+    }
+  };
+
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={onSubmit}>
+          <InputFileType data={data} setData={setData} />
+          <TextFieldFasta data={data} setData={setData} />
+          <InputFileFasta data={data} setData={setData} />
+          <ButtonRun data={data} />
+        </form>
+      </FormContainer>
+    </>
+  );
+}
--- frontend/src/pages/frequency.tsx
+++ frontend/src/pages/frequency.tsx
@@ -1,12 +1,18 @@
+import FrequencyForm from "../components/frequency/frequency_form";
 import Layout from "../components/layout";
+import SectionTitle from "../components/section_title";
 import { useHandleSection } from "../hooks/useHandleSection";
 
 export default function Frequency() {
-  useHandleSection({section: "frequency"})
+  useHandleSection({ section: "frequency" });
 
   return (
     <Layout>
-      <h1>Frequency</h1>
+      <>
+        <SectionTitle title="Frequency Evaluation" />
+
+        <FrequencyForm />
+      </>
     </Layout>
   );
 }
