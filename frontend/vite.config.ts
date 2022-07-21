d360be9c76d755236c4093b6520acf926d2344ce
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-07-21 13:42:52 -0400

    Actualizando el docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": "http://190.114.255.125:8001",
-  //     "/files": "http://190.114.255.125:8001"
-  //   }
-  // }
+  server: {
+    proxy: {
+      "/api": "http://66.94.118.108:8001",
+      "/files": "http://66.94.118.108:8001"
+    }
+  }
 })
