3dd70147e773b461b99565849327507b98c2ed96
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-07-26 16:25:16 -0400

    Corrigiendo go y pfam

--- frontend/src/components/msa/msa_content.tsx
+++ frontend/src/components/msa/msa_content.tsx
@@ -1,10 +1,9 @@
 import { Box, Paper } from "@mui/material";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { IAlign } from "../../utils/interfaces";
 import BackdropComponent from "../backdrop_component";
 import ButtonDownloadPrimary from "../button_download_primary";
 import ProSeqViewer from "../pro_seq_viewer";
-import Plot from "react-plotly.js";
 
 interface Props {
   result: IAlign;
@@ -49,34 +48,6 @@ export default function MSAContent({ result }: Props) {
           setOpenBackdrop={setOpenBackdrop}
           setPercentage={setPercentage}
         />
-      </Box>
-      <Box marginTop={3} boxShadow={4}>
-        <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-          <Plot
-            data={[
-              {
-                x: result.distance_heatmap.x,
-                y: result.distance_heatmap.y,
-                z: result.distance_heatmap.z,
-                type: "heatmap",
-              },
-            ]}
-            layout={{
-              autosize: true,
-              height: 600,
-              title: "Distances Matrix",
-              font: {
-                size: 15,
-              },
-            }}
-            config={{
-              displayModeBar: false,
-              responsive: true,
-            }}
-            useResizeHandler
-            className="w-full h-full"
-          />
-        </Paper>
       </Box>
     </>
   );
