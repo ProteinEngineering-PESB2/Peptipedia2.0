32d50aae5f0218a3604a921146e32a2be5108088
Diego Alvarez S <dialvarezs@gmail.com>
2022-09-21 06:46:22 -0300

    Fix last issues with static files

--- backend/peptipedia/wsgi.py
+++ backend/peptipedia/wsgi.py
@@ -10,11 +10,7 @@ from peptipedia.modules.utils import Folders
 f = Folders("config.ini")
 f.create_folders()
 
-app = Flask(
-    __name__,
-    static_url_path="/files",
-    static_folder=os.path.realpath(f.get_static_folder()),
-)
+app = Flask(__name__, static_folder=os.path.realpath(f.get_static_folder()))
 # Cors
 CORS(app)
 
