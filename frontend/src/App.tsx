b6ba27730d938697c2dcfd46e40622170496e4c3
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2023-03-26 18:19:44 -0300

    Borrando servicios de pepti tools

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,28 +7,16 @@ import AppProvider from "./context/AppProvider";
 import Home from "./pages/home";
 import Database from "./pages/database";
 import AdvancedSearch from "./pages/advanced_search";
-import FastaConverter from "./pages/fasta_converter";
 import AlignmentSequence from "./pages/alignment_sequence";
-import MultiAlignmentSequence from "./pages/msa";
-import Pfam from "./pages/pfam";
-import GeneOntology from "./pages/gene_ontology";
-import Frequency from "./pages/frequency";
-import Physicochemical from "./pages/physicochemical";
-import Encoding from "./pages/encoding";
-import Clustering from "./pages/clustering";
-import SupervisedLearning from "./pages/supervised_learning";
 import { Routes, Route } from "react-router-loading";
 import LoadingComponent from "./components/Loading";
 import PeptideDetail from "./pages/peptipe_detail";
 import Team from "./pages/Team";
 import HowToCite from "./pages/HowToCite";
 import NotFound from "./pages/NotFound";
-import StructuralPrediction from "./pages/StructuralPrediction";
-import SequencesGenerator from "./pages/SequencesGenerator";
 import Actividades from "./pages/Actividades";
 import Promiscuous from "./pages/Promiscuous";
 import ActivityDetail from "./pages/ActivityDetail";
-import ActivityPrediction from "./pages/ActivityPrediction";
 
 export default function App() {
   const theme = createTheme();
@@ -49,39 +37,8 @@ export default function App() {
                 loading
               />
               <Route
-                path="/fasta-converter"
-                element={<FastaConverter />}
-                loading
-              />
-              <Route
-                path="/test-sequences"
-                element={<SequencesGenerator />}
-                loading
-              />
-              <Route
                 path="/alignment-sequence"
                 element={<AlignmentSequence />}
-                loading
-              />
-              <Route path="/msa" element={<MultiAlignmentSequence />} loading />
-              <Route path="/pfam" element={<Pfam />} loading />
-              <Route path="/gene-ontology" element={<GeneOntology />} loading />
-              <Route path="/frequency" element={<Frequency />} loading />
-              <Route
-                path="/structural-prediction"
-                element={<StructuralPrediction />}
-                loading
-              />
-              <Route
-                path="/physicochemical"
-                element={<Physicochemical />}
-                loading
-              />
-              <Route path="/encoding" element={<Encoding />} loading />
-              <Route path="/clustering" element={<Clustering />} loading />
-              <Route
-                path="/supervised-learning"
-                element={<SupervisedLearning />}
                 loading
               />
               <Route
@@ -94,11 +51,6 @@ export default function App() {
               <Route
                 path="/activity/:activityId"
                 element={<ActivityDetail />}
-                loading
-              />
-              <Route
-                path="/activity-prediction"
-                element={<ActivityPrediction />}
                 loading
               />
               <Route path="*" element={<NotFound />} loading />
